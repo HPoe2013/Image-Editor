ef21063ff34d66365cf8cd3ff87ea7b6fd623f35
Quentin 'Poe' Caffero <qcaffero@gmail.com>
2019-10-22 14:54:05 -0500

    Updated: Send event to enable save on open

--- electron-scripts/preload.js
+++ electron-scripts/preload.js
@@ -1,6 +1,8 @@
 const { ipcRenderer } = require('electron');
 
 ipcRenderer.on('file-open', (event, file) => {
+	ipcRenderer.send('enable', 'saveItem');
+
 	let evt = new window.CustomEvent(
 		'file-open',
 		{
