df551f84f9a263f8747616997d8841d947fbdc04
Quentin 'Poe' Caffero <qcaffero@gmail.com>
2019-10-22 14:52:29 -0500

    Updated: Save Dialog functionality

--- electron-scripts/file-controller.js
+++ electron-scripts/file-controller.js
@@ -1,9 +1,6 @@
 const { dialog } = require('electron');
-const fileSys = require('fs');
 
 module.exports = new function () {
-	this.load = function (file) {};
-
 	this.openDialog = function (win, opts) {
 		let options = {
 			title: 'Open...',
@@ -16,6 +13,21 @@ module.exports = new function () {
 			dialog.showOpenDialog(win, combinedOpts, (files) => {
 				let file = files[0] != null ? files[0] : null;
 				resolve(file);
+			});
+		});
+	};
+
+	this.saveDialog = function (win, opts) {
+		let options = {
+			title: 'Save...',
+			buttonLabel: 'Save'
+		};
+
+		let combinedOpts = Object.assign(options, opts);
+
+		return new Promise((resolve, reject) => {
+			dialog.showSaveDialog(win, combinedOpts, file => {
+				console.log('file', file);
 			});
 		});
 	};
