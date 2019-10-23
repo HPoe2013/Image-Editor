d48cef87550b929e18ac05d8e4f3cb3ec5d6fff0
Quentin 'Poe' Caffero <qcaffero@gmail.com>
2019-10-23 11:47:02 -0500

    Updated: Respond to events and ability to save file

--- electron-scripts/file-controller.js
+++ electron-scripts/file-controller.js
@@ -1,4 +1,17 @@
-const { dialog } = require('electron');
+const { dialog, ipcMain } = require('electron');
+const BSON = require('bson');
+
+const fileSys = require('fs');
+const path = require('path');
+
+let loadProjFile = function (filePath) {
+	return new Promise((resolve, reject) => {
+		fileSys.readFile(filePath, (err, data) => {
+			if (err) reject(err);
+			else resolve(BSON.deserialize(data));
+		});
+	});
+};
 
 module.exports = new function () {
 	this.openDialog = function (win, opts) {
@@ -11,8 +24,18 @@ module.exports = new function () {
 
 		return new Promise((resolve, reject) => {
 			dialog.showOpenDialog(win, combinedOpts, (files) => {
+				let isProj = false;
 				let file = files[0] != null ? files[0] : null;
-				resolve(file);
+
+				if (file != null) {
+					isProj = path.extname(file) === '.poe';
+				}
+
+				if (isProj) {
+					loadProjFile(file).then(data => {
+						resolve({isProj: true, file: data});
+					});
+				} else resolve({isProj: false, file});
 			});
 		});
 	};
@@ -26,9 +49,20 @@ module.exports = new function () {
 		let combinedOpts = Object.assign(options, opts);
 
 		return new Promise((resolve, reject) => {
-			dialog.showSaveDialog(win, combinedOpts, file => {
-				console.log('file', file);
+			dialog.showSaveDialog(win, combinedOpts, resolve);
+		});
+	};
+
+	this.saveFile = function (path, data) {
+		return new Promise((resolve, reject) => {
+			fileSys.writeFile(path, data, err => {
+				if (err) reject(err);
+				else resolve();
 			});
 		});
 	};
+
+	ipcMain.on('save-file', (evt, data) => {
+		this.saveFile(data.file, BSON.serialize(data.data));
+	});
 }();
