426c25f8e95360f48131154cc1835aacf6bde76a
Quentin 'Poe' Caffero <qcaffero@gmail.com>
2019-10-24 13:49:05 -0500

    Added: Script to handle user actions

---
+++ www/scripts/action-handler.js
@@ -0,0 +1,42 @@
+import Toolbox from './toolbox';
+import ToolKit from './tools/toolkit';
+
+export default function () {
+	this._frame = null;
+	this._panZoom = null;
+	this._layers = null;
+
+	this._active = false;
+
+	this._lastMouseCoord = null;
+
+	let _ctor = function (frame, panZoomCtrl, layerCtrl) {
+		this._frame = frame;
+		this._panZoom = panZoomCtrl;
+		this._layers = layerCtrl;
+
+		this._frame.addEventListener('mousedown', _handleMouseDown.bind(this));
+		this._frame.addEventListener('mousemove', _handleMouseMove.bind(this));
+		this._frame.addEventListener('mouseup', _handleMouseUp.bind(this));
+	};
+
+	let _handleMouseDown = function (e) {
+		let tool = Toolbox.getActiveTool();
+
+		console.log('tool', tool, ToolKit[tool]);
+
+		ToolKit[tool].default.mousedown.call(this, e);
+	};
+
+	let _handleMouseMove = function (e) {
+		let tool = Toolbox.getActiveTool();
+		ToolKit[tool].default.mousemove.call(this, e);
+	};
+
+	let _handleMouseUp = function (e) {
+		let tool = Toolbox.getActiveTool();
+		ToolKit[tool].default.mouseup.call(this, e);
+	};
+
+	_ctor.apply(this, arguments);
+};
