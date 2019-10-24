04294addec197ecee52f1a04e2d4e14d3c8209bb
Quentin 'Poe' Caffero <qcaffero@gmail.com>
2019-10-24 13:47:06 -0500

    Updated: Build toolkit file

--- build/build-dev.sh
+++ build/build-dev.sh
@@ -1,5 +1,30 @@
+#!/bin/bash
+
+build_toolkit_file () {
+	touch ./www/scripts/tools/toolkit.js;
+
+	exStr="export default { ";
+
+	echo "" > ./www/scripts/tools/toolkit.js;
+
+	for file in ./www/scripts/tools/*; do
+		if [ $(basename "$file") != "toolkit.js" ] ; then
+			filename=$(basename "$file" .js);
+			exStr+="$filename: $filename, "
+			echo "const $filename = require('./$filename');" >> ./www/scripts/tools/toolkit.js;
+		fi
+	done
+
+	outStr="${exStr::-2} };"
+	echo "$outStr" >> ./www/scripts/tools/toolkit.js;
+}
+
 echo "Cleaning dist directory...";
 rm -rf dist/*;
+
+echo "";
+echo "Building tool file...";
+build_toolkit_file;
 
 echo "";
 echo "Webpacking front end scripts...";
@@ -10,6 +35,8 @@ echo "Copying other files to dist...";
 cp ./www/index.html ./dist/www/index.html;
 cp -r ./www/styles	./dist/www/styles
 cp -r ./www/images	./dist/www/images
+
+rm ./www/scripts/tools/toolkit.js;
 
 echo "";
 NOW=$(date +"%T");
