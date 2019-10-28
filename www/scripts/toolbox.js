338524a8bc0bdfe8d2b46e7eeede10400bb8b509
Quentin 'Poe' Caffero <qcaffero@gmail.com>
2019-10-28 09:24:47 -0500

    Updated: Handling parameter box

--- www/scripts/toolbox.js
+++ www/scripts/toolbox.js
@@ -1,3 +1,5 @@
+import ToolKit from './tools/toolkit';
+
 /** Class to handle the toolbar on the front end. */
 export default new function () {
 	this._toolbox = null;	// DOM Node housing the tool selection.
@@ -8,12 +10,16 @@ export default new function () {
 	 * @param  {DOM Node} selTool The node for the selected tool
 	 */
 	let _activateTool = function (selTool) {
+		// TODO: Save current param settings in order to reload them on tool selection?
+
 		let activeTools = this._toolbox.querySelectorAll('.active');
 		Array.prototype.forEach.call(activeTools, (tool) => {
 			tool.classList.remove('active');
 		});
 
 		selTool.classList.add('active');
+
+		_setupParams.call(this, selTool.dataset.tool);
 	};
 
 	/**
@@ -24,10 +30,34 @@ export default new function () {
 		_activateTool.call(this, e.target);
 	};
 
+	let _setupParams = function (tool) {
+		this._currToolParams = ToolKit[tool].params;
+
+		if (this._currToolParams == null) {
+			this._toolParamBox.classList.add('hidden');
+			return;
+		}
+
+		this._toolParamBox.classList.remove('hidden');
+
+		let props = this._toolParamBox.querySelectorAll('.prop');
+		Array.prototype.forEach.call(props, prop => {
+			if (this._currToolParams[prop.dataset.prop] != null) {
+				prop.classList.remove('hidden');
+
+				let pData = this._currToolParams[prop.dataset.prop];
+				pData.init.call(this, prop, pData);
+			} else prop.classList.add('hidden');
+		});
+	};
+
 	/** Initialization function to set up toolbar. */
 	this.init = function () {
 		this._toolbox = document.querySelector('#toolbar #tools');
 		this._colorBox = document.querySelector('#toolbar #colors');
+		this._toolParamBox = document.querySelector('#toolbar #tool-params');
+
+		this._toolbox.parentNode.classList.remove('hidden');
 
 		let tools = this._toolbox.querySelectorAll('.tool-icon');
 		Array.prototype.forEach.call(tools, (tool) => {
@@ -52,5 +82,9 @@ export default new function () {
 	 */
 	this.getMainColor = function () {
 		return this._colorBox.querySelector('#main').value;
+	};
+
+	this.getParamBox = function (prop) {
+		return this._toolParamBox.querySelector('[data-prop="' + prop + '"]');
 	};
 }();
